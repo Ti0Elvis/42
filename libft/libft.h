8b5addc572a6e07acb9922599471c004eba36a58
Ti0Elvis <elvis.veramijares@gmail.com>
2025-06-04 15:19:05 +0000

    Add character classification functions; Add .gitignore file

--- libft/libft.h
+++ libft/libft.h
@@ -1,4 +1,11 @@
 #ifndef LIBFT_H
 #define LIBFT_H
 
+// Function prototypes for character classification functions
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+
 #endif
